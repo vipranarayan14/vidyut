29551667831215544fa5ff51c0b36880bfbd0e4d
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2025-04-02 06:42:06 +0000

    add padi info to stem suffix extractor script

--- vidyut-prakriya/examples/create_all_tinantas.rs
+++ vidyut-prakriya/examples/create_all_tinantas.rs
@@ -13,7 +13,7 @@ use serde::Serialize;
 use std::error::Error;
 use std::io;
 use vidyut_lipi::{Lipika, Scheme};
-use vidyut_prakriya::args::{Lakara, Prayoga, Purusha, Sanadi, Tinanta, Vacana};
+use vidyut_prakriya::args::{DhatuPada, Lakara, Prayoga, Purusha, Sanadi, Tinanta, Vacana};
 use vidyut_prakriya::{Dhatupatha, Vyakarana};
 
 /// Command line arguments.
@@ -33,11 +33,13 @@ struct Row<'a> {
     dhatu: &'a str,
     gana: &'static str,
     number: u16,
+    // code: String,
+    sanadis: String,
     prayoga: &'static str,
+    padi: &'static str,
     lakara: &'static str,
     purusha: &'static str,
     vacana: &'static str,
-    sanadis: String,
 }
 
 fn create_pada_string(mut padas: Vec<String>, output_scheme: Scheme) -> String {
@@ -83,47 +85,52 @@ fn run(dhatupatha: Dhatupatha, args: Args) -> Result<(), Box<dyn Error>> {
 
         for sanadis in &sanadi_choices {
             for prayoga in &[Prayoga::Kartari, Prayoga::Karmani] {
-                for lakara in Lakara::iter() {
-                    for purusha in Purusha::iter() {
-                        for vacana in Vacana::iter() {
-                            let tinanta = Tinanta::builder()
-                                .dhatu(dhatu.clone().with_sanadi(&sanadis))
-                                .prayoga(*prayoga)
-                                .purusha(*purusha)
-                                .vacana(*vacana)
-                                .lakara(*lakara)
-                                .build()?;
+                for dhatupada in DhatuPada::iter() {
+                    for lakara in Lakara::iter() {
+                        for purusha in Purusha::iter() {
+                            for vacana in Vacana::iter() {
+                                let tinanta = Tinanta::builder()
+                                    .dhatu(dhatu.clone().with_sanadi(&sanadis))
+                                    .prayoga(*prayoga)
+                                    .pada(*dhatupada)
+                                    .lakara(*lakara)
+                                    .purusha(*purusha)
+                                    .vacana(*vacana)
+                                    .build()?;
 
-                            let prakriyas = v.derive_tinantas(&tinanta);
-                            if prakriyas.is_empty() {
-                                continue;
-                            }
+                                let prakriyas = v.derive_tinantas(&tinanta);
+                                if prakriyas.is_empty() {
+                                    continue;
+                                }
 
-                            let dhatu_text = &dhatu.upadesha().expect("ok");
-                            let padas: Vec<_> = prakriyas.iter().map(|p| p.text2()).collect();
+                                let dhatu_text = &dhatu.upadesha().expect("ok");
+                                let padas: Vec<_> = prakriyas.iter().map(|p| p.text2()).collect();
 
-                            for pada in padas {
-                                let padas = create_pada_string(vec![pada], output_scheme);
-                                let sanadi: Vec<String> = sanadis
-                                    .iter()
-                                    .map(|s| s.as_str().to_owned()) // Convert to `String` to own the data
-                                    .collect();
+                                for pada in padas {
+                                    let padas = create_pada_string(vec![pada], output_scheme);
+                                    let sanadi: Vec<String> = sanadis
+                                        .iter()
+                                        .map(|s| s.as_str().to_owned()) // Convert to `String` to own the data
+                                        .collect();
 
-                                let joined_sanadi: String = sanadi.join(":"); // Now you can safely join
+                                    let joined_sanadi: String = sanadi.join(":"); // Now you can safely join
 
-                                let row = Row {
-                                    padas,
-                                    dhatu: dhatu_text,
-                                    gana: dhatu.gana().expect("ok").as_str(),
-                                    number: entry.number(),
-                                    lakara: lakara.as_str(),
-                                    purusha: purusha.as_str(),
-                                    vacana: vacana.as_str(),
-                                    prayoga: prayoga.as_str(),
-                                    sanadis: joined_sanadi,
-                                };
+                                    let row = Row {
+                                        padas,
+                                        dhatu: dhatu_text,
+                                        gana: dhatu.gana().expect("ok").as_str(),
+                                        number: entry.number(),
+                                        // code: entry.code().to_owned(),
+                                        sanadis: joined_sanadi,
+                                        lakara: lakara.as_str(),
+                                        padi: dhatupada.as_str(),
+                                        prayoga: prayoga.as_str(),
+                                        purusha: purusha.as_str(),
+                                        vacana: vacana.as_str(),
+                                    };
 
-                                wtr.serialize(row)?;
+                                    wtr.serialize(row)?;
+                                }
                             }
                         }
                     }
